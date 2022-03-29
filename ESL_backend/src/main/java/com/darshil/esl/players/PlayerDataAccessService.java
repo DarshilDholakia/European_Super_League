077c64ab393ad419733aaefdafbf6568083b091b
Suraaj Leal <suraaj_leal@hotmail.co.uk>
2022-03-29 11:17:18 +0100

    added annotation to playerservice class

--- ESL_backend/src/main/java/com/darshil/esl/players/PlayerDataAccessService.java
+++ ESL_backend/src/main/java/com/darshil/esl/players/PlayerDataAccessService.java
@@ -1,4 +1,7 @@
 package com.darshil.esl.players;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
 public class PlayerDataAccessService {
 }
--- ESL_backend/src/main/java/com/darshil/esl/players/PlayerService.java
+++ ESL_backend/src/main/java/com/darshil/esl/players/PlayerService.java
@@ -1,4 +1,13 @@
 package com.darshil.esl.players;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PlayerService {
+
+    private PlayerDao playerDao;
+
+    public PlayerService(@Qualifier("player_postgres") PlayerDao playerDao){
+        this.playerDao = playerDao;
+    }
 }
