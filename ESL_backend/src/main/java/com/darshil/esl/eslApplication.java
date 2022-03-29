4869f33bb65b4c7a1e89a982006032a9dd3cddeb
Adib Baig <adibzbaig@gmail.com>
2022-03-29 10:04:37 +0100

    added springboot dependencies

--- ESL_backend/src/main/java/com/darshil/esl/eslApplication.java
+++ ESL_backend/src/main/java/com/darshil/esl/eslApplication.java
@@ -1,4 +1,11 @@
 package com.darshil.esl;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class eslApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(eslApplication.class, args);
+    }
 }
