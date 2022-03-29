ac296480ce7a1480fe9d06d0184b0b0e0fff0da3
FIRST_NAME LAST_NAME <abdiafrah1997@outlook.com>
2022-03-29 10:42:30 +0100

    inital commit

--- ESL_backend/src/main/java/com/darshil/esl/users/User.java
+++ ESL_backend/src/main/java/com/darshil/esl/users/User.java
@@ -1,4 +1,11 @@
 package com.darshil.esl.users;
 
 public class User {
+    private String email;
+    private String password;
+    private String teamName;
+    private Integer totalPoint;
+    private List <Players> players;
+
+
 }
