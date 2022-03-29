321769f4cfee61a374853cac78e10163a3190fcd
FIRST_NAME LAST_NAME <abdiafrah1997@outlook.com>
2022-03-29 14:34:08 +0100

    completed custom exceptions

--- ESL_backend/src/main/java/com/darshil/esl/users/user_exceptions/EmailInvalidException.java
+++ ESL_backend/src/main/java/com/darshil/esl/users/user_exceptions/EmailInvalidException.java
@@ -1,4 +1,12 @@
 package com.darshil.esl.users.user_exceptions;
 
-public class EmailInvalidException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class EmailInvalidException extends IllegalStateException {
+
+    public EmailInvalidException(String message) {
+        super(message);
+    }
 }
--- ESL_backend/src/main/java/com/darshil/esl/users/user_exceptions/RowNotChangedException.java
+++ ESL_backend/src/main/java/com/darshil/esl/users/user_exceptions/RowNotChangedException.java
@@ -1,4 +1,12 @@
 package com.darshil.esl.users.user_exceptions;
 
-public class RowNotChangedException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class RowNotChangedException extends IllegalStateException{
+
+    public RowNotChangedException(String message) {
+        super(message);
+    }
 }
