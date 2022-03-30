f9e96ec1ad17bf04e3de4460c2720ccd484b5dfc
Aaron Nazareth <aaronnazareth@hotmail.co.uk>
2022-03-30 20:01:56 +0100

    Added initial router setup for page switching in React App

---
+++ esl_frontend/src/components/AdminHub.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminHub = () => {
+
+    return(
+        <h1>AdminHub here</h1>
+    )
+
+}
+
+export default AdminHub;
---
+++ esl_frontend/src/components/SignupPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignupPage = () => {
+
+    return(
+        <h1>Sign up here</h1>
+    )
+
+}
+
+export default SignupPage;
