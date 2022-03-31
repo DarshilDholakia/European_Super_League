24616a6cd4d834453eb616739260313392858ac8
Michael Atayero <michaelak1395@gmail.com>
2022-03-31 12:34:48 +0100

    Player functions added

--- esl_frontend/src/components/BuildTeam.js
+++ esl_frontend/src/components/BuildTeam.js
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from 'react';
 const BuildTeam = () => {
 
     const [assignmentList, setAssignmentList] = useState([])
+    const [playerNameFilter, setPlayerNameFilter] = useState([])
+    const [playerPositionFilter, setPlayerPositionFilter] = useState([])
+    const [playerClubFilter, setPlayerClubFilter] = useState([])
 
     const fetchAllAssignments = () => {
         fetch("http://localhost:8080/assignments/all")
@@ -38,6 +41,35 @@ const BuildTeam = () => {
         .then(() => fetchAllAssignments())
         .catch((error) => console.error(error))
     };
+
+    //PLAYER FUNCTIONS 
+
+    const fetchPlayerbyName = (name) => {
+        fetch(`http://localhost:8080/player/name/${name}`)
+        .then((response) => response.json())
+        .then(data => setPlayerNameFilter(data))
+        .catch((error) => console.error(error))
+    }
+
+
+    const fetchPlayerbyPosition = (player_position) => {
+        fetch(`http://localhost:8080/player/position/${player_position}`)
+        .then((response) => response.json())
+        .then(data => setPlayerPositionFilter(data))
+        .catch((error) => console.error(error))
+    }
+
+    const fetchPlayerbyClub = (player_club) => {
+        fetch(`http://localhost:8080/player/club/${player_club}`)
+        .then((response) => response.json())
+        .then(data => setPlayerClubFilter(data))
+        .catch((error) => console.error(error))
+    }
+    
+        
+
+    
+    
 
 
 
