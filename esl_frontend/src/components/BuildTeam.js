c5db156821fe2f4433f1f1e0a85f548557590d0c
Suraaj Leal <suraaj_leal@hotmail.co.uk>
2022-04-03 20:52:31 +0100

    remove extra test function

--- esl_frontend/src/components/BuildTeam.js
+++ esl_frontend/src/components/BuildTeam.js
@@ -432,21 +432,6 @@ const BuildTeam = ({ playerList }) => {
         }
     }
 
-    function dataURLtoFile(dataurl, filename) {
- 
-        var arr = dataurl.split(','),
-            mime = arr[0].match(/:(.*?);/)[1],
-            bstr = atob(arr[1]), 
-            n = bstr.length, 
-            u8arr = new Uint8Array(n);
-            
-        while(n--){
-            u8arr[n] = bstr.charCodeAt(n);
-        }
-        
-        return new File([u8arr], filename, {type:mime});
-    }
-
     return (
         <>
             <h1>Assemble your squad</h1>
