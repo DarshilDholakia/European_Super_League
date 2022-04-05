0d87a89839af83bdb21838feddc2c08b91135974
Adib Baig <adibzbaig@gmail.com>
2022-04-05 12:21:47 +0100

    added sign out button css

--- esl_frontend/src/components/AdminNavBar.js
+++ esl_frontend/src/components/AdminNavBar.js
@@ -1,13 +1,17 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "./UserContext";
+import bntaLogo from '../assets/bntaLogo.png'
 
 const AdminNavBar = () => {
 
-    const {admin, adminLogout} = useContext(UserContext);
+    const { admin, adminLogout } = useContext(UserContext);
 
-    return(
+    return (
         <ul className="navbar">
+            <li>
+                <img src={bntaLogo} alt="bnta logo" width="75" height="100" />
+            </li>
             <li>
                 <Link to="/"> Home </Link>
             </li>
@@ -15,7 +19,7 @@ const AdminNavBar = () => {
                 <Link to="/admin-hub"> AdminHub </Link>
             </li>
             <li>
-                <button onClick={adminLogout}>Sign Out</button>
+                <button className="sign-out-button" onClick={adminLogout}>Sign Out</button>
             </li>
         </ul>
     )
